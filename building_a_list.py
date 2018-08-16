9134da541ee900baf0d984bdc05225293ed80f04
sushmita1991 <suanarim@gmail.com>
2018-08-16 18:05:05 +0200

    adding first commit in local repo

---
+++ building_a_list.py
@@ -0,0 +1,9 @@
+stuff=list()# a constructor that says give me an empty list
+stuff.append('book') # append at the end of the book a list
+stuff.append(99) # append 99 after book
+print (stuff)
+stuff.append('cookie')
+print (stuff)
+num=[0,20,30,40,50]
+new_num=num[:]
+num is new_num
